c2c909398caa210f520a3192da4001550f37ee00
Your Name <you@example.com>
2026-02-10 23:58:36 +0900

    Fix Express v5 catch-all with named capture group

--- server/index.cjs
+++ server/index.cjs
@@ -113,7 +113,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/:path*', (req, res) => {
+app.get('/:any(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
